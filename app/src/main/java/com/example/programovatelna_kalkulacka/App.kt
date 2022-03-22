a1683666698929490e936a207bc96556d59f665a
viktorkaderabek <viktorkaderabekpc@seznam.cz>
2022-03-23 00:01:53 +0100

    Projekt

--- app/src/main/java/com/example/programovatelna_kalkulacka/App.kt
+++ app/src/main/java/com/example/programovatelna_kalkulacka/App.kt
@@ -5,17 +5,13 @@ import com.example.programovatelna_kalkulacka.di.viewModelModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
-class App : Application()
-{
-
-    override fun onCreate()
-    {
-        super.onCreate()
-        startKoin {
-            androidContext(this@App)
-            modules(listOf(viewModelModule))
-        }
-
-    }
-
+class App : Application() {
+     
+     override fun onCreate() {
+          super.onCreate()
+          startKoin {
+               androidContext(this@App)
+               modules(listOf(viewModelModule))
+          }
+     }
 }
--- app/src/main/java/com/example/programovatelna_kalkulacka/di/AppModule.kt
+++ app/src/main/java/com/example/programovatelna_kalkulacka/di/AppModule.kt
@@ -5,9 +5,8 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
-
-    viewModel {
-        MainViewModel()
-    }
+     viewModel {
+          MainViewModel()
+     }
 }
 
